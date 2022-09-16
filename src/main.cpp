583ca763f9ff9f02140b0485c9ba5f4088e5c774
BriefLemon <20191279@game.hoseo.edu>
2022-09-16 13:51:49 +0900

    HelloSDL-분리하기
    20220916 실습 + 추가실습

--- src/main.cpp
+++ src/main.cpp
@@ -2,39 +2,58 @@
 //
 // written by changhoonpark@gmail.com
 
-
-#include "main.h"
-
+#include <SDL2/SDL.h>
+#include <stdlib.h>
 
 SDL_Window*		g_pWindow = 0;
 SDL_Renderer*	g_pRenderer = 0;
+bool g_bRunning = false; //게임이 Run 하고 있는지 여부
 
-int main(int argc, char* args[])
+bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
 {
-
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
-
-		g_pWindow = SDL_CreateWindow("Game Framework",
-			SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED, 300, 300,
-			SDL_WINDOW_BORDERLESS|SDL_WINDOW_MAXIMIZED);
-
+		g_pWindow = SDL_CreateWindow(
+			title, xpos, ypos, height, width, flags);
 
 		if (g_pWindow != 0) {
-			g_pRenderer
-				= SDL_CreateRenderer(g_pWindow, -1, 0);
+			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
 		}
+
+	} else {
+		return false;
+	}
+
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+
+	return true;
+}
+
+void render()
+{
+	SDL_RenderClear(g_pRenderer);
+
+	SDL_SetRenderDrawColor(g_pRenderer, rand() % 256, rand() % 256, rand() % 256, 255);
+	SDL_Delay(1000);
+
+	SDL_RenderPresent(g_pRenderer);
+}
+
+int main(int argc, char* argv[])
+{
+	if (init("Breaking Up HelloSDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		640, 480, SDL_WINDOW_SHOWN))
+	{
+		g_bRunning = true;
 	}
 	else {
-		return 1;
+		return 1; //Something went wrong
 	}
 
-	SDL_SetRenderDrawColor(
-		g_pRenderer, 0, 0, 255, 255);
-	SDL_RenderClear(g_pRenderer);
-	SDL_RenderPresent(g_pRenderer);
+	while (g_bRunning) {
+		//handle input - update - render
+		render();
+	}
 
-	SDL_Delay(10000);
 	SDL_Quit();
 
 	return 0;
