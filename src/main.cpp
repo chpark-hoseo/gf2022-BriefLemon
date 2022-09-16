64413b3c894d699c7e91904043160e15c50341d7
BriefLemon <20191279@game.hoseo.edu>
2022-09-16 15:17:25 +0900

    HelloSDL-분리하기-과제
    1. main.cpp 에서 main()함수가  다른 함수보다 가장 먼저 나올 수 있도록 코드 수정하기
    
    2. 무한루프 빠져나오도록 코드 수정

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,40 @@
 
 #include <SDL2/SDL.h>
 #include <stdlib.h>
+#include <conio.h>
 
 SDL_Window*		g_pWindow = 0;
 SDL_Renderer*	g_pRenderer = 0;
-bool g_bRunning = false; //게임이 Run 하고 있는지 여부
+bool g_bRunning = false; //프로그램이 Run 하고 있는지 여부
+const int runTime = 10000; //프로그램 종료까지 시간(ms 단위)
+
+int main(int argc, char* argv[])
+{
+	bool init(const char* title, int xpos, int ypos, int height, int width, int flags);
+	void render();
+
+	if (init("Breaking Up HelloSDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		640, 480, SDL_WINDOW_SHOWN))
+	{
+		g_bRunning = true;
+	}
+	else {
+		return 1; //Something went wrong
+	}
+
+	while (g_bRunning) {
+		//handle input - update - render
+		render();
+
+		if (SDL_GetTicks() > runTime) { //코드가 실행된 시간을 ms 단위로 표현
+			g_bRunning = false;
+		}
+	}
+
+	SDL_Quit();
+
+	return 0;
+}
 
 bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
 {
@@ -36,25 +66,4 @@ void render()
 	SDL_Delay(1000);
 
 	SDL_RenderPresent(g_pRenderer);
-}
-
-int main(int argc, char* argv[])
-{
-	if (init("Breaking Up HelloSDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		640, 480, SDL_WINDOW_SHOWN))
-	{
-		g_bRunning = true;
-	}
-	else {
-		return 1; //Something went wrong
-	}
-
-	while (g_bRunning) {
-		//handle input - update - render
-		render();
-	}
-
-	SDL_Quit();
-
-	return 0;
 }
